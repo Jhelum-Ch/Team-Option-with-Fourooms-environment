62fecc362419cb612c2c64ab03674deed7302342
sumanabasu <sumana.basu21@gmail.com>
2019-01-05 01:23:43 -0500

    more parameters added

---
+++ modelConfig.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+params = {
+	'env'	:	{
+		'n_agents'			:	3,
+		'goal_reward' 		:	 1,
+		'broadcast_penalty'	:  -0.01,
+		'collision_penalty' :  -0.01
+	},
+	'agent'	:	{
+		'n_options'	:	5
+	},
+	'policy'	:	{
+		'temperature'	: 0.5
+	},
+	'train': {
+		'n_epochs'		: 	50,
+		'n_episodes'	:	100,
+		'n_steps' 		: 	100,
+		'seed'			:	np.random.RandomState(1234)
+	},
+	
+	'doc'	:	{
+		'lr_theta'	: 0.01,
+		'lr_phi'	: 0.01
+	}
+}
