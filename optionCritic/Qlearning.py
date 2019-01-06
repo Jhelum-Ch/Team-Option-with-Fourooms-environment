e0f7411ddee3b805462d11e1b09caa164b96b403
sumanabasu <sumana.basu21@gmail.com>
2019-01-06 01:11:47 -0500

    IntraOptionActionQLearning : final version

--- optionCritic/Qlearning.py
+++ optionCritic/Qlearning.py
@@ -50,7 +50,6 @@ class IntraOptionQLearning:
 		# calculates termination probability of at least one agent
 		prod = 1.0
 		for idx in range(self.n_agents):
-			# prod *= 1 - self.terminations[self.agents[idx].option].pmf(joint_state[idx])
 			prod *= 1 - self.terminations[joint_option[idx]].pmf(joint_state[idx])
 			
 		return 1.0 - prod
@@ -67,10 +66,10 @@ class IntraOptionQLearning:
 		update_target = reward	#delta
 		current_Q = self.getQvalue(joint_state, joint_option)
 		if not done:
-			beta = self.terminationProbOfAtLeastOneAgent(joint_state, joint_option) # 1-beta is the probability that
-			# none of the agents terminate. Hence, the current option continues.
-			update_target += self.discount*((1. - beta)*current_Q +
-											beta * self.getQvalue(joint_state, None, joint_option))
+			beta = self.terminationProbOfAtLeastOneAgent(joint_state, joint_option) # (1 - beta) is the probability
+			# that none of the agents terminate. Hence, the current option continues.
+			update_target += self.discount*((1. - beta)*current_Q + beta * self.getQvalue(joint_state, None,
+																						  joint_option))
 			
 			self.last_value = current_Q
 			self.last_joint_option = joint_option
@@ -92,7 +91,7 @@ class IntraOptionActionQLearning:
 		self.weights = weights
 		self.qbigomega = qbigomega
 		
-	def start(self, joint_state, joint_option):
+	def start(self, joint_state, joint_option, joint_action):
 		'''
 		:param joint_state: tuple of state encodings. Ranges from (0, 0, 0) to (103, 103, 103)
 		:param joint_option:
@@ -100,33 +99,34 @@ class IntraOptionActionQLearning:
 		'''
 		self.last_joint_state = joint_state
 		self.last_joint_option = joint_option
-		self.last_value = self.getQvalue(joint_state, joint_option)
+		self.last_joint_action = joint_action
 
-	def getQvalue(self, joint_state, joint_option, action):	#TODO: fix this
-		return self.weights[joint_state][joint_option][action]
+	def getQvalue(self, joint_state, joint_option, joint_action):
+		return self.weights[joint_state][joint_option][joint_action]
 	
 	def terminationProbOfAtLeastOneAgent(self, joint_state, joint_option):
 		# calculates termination probability of at least one agent
 		prod = 1.0
 		for idx in range(self.n_agents):
-			# prod *= 1 - self.terminations[self.agents[idx].option].pmf(joint_state[idx])
 			prod *= 1 - self.terminations[joint_option[idx]].pmf(joint_state[idx])
-		
+
 		return 1.0 - prod
 
-	def update(self, phi, joint_option, joint_action, reward, done):	#TODO: fix this
+	def update(self, joint_state, joint_option, joint_action, reward, done):
 		# One-step update target
 		update_target = reward
 		if not done:
-			current_values = self.qbigomega.value(phi)
-			termination = self.terminations[self.last_options].pmf(Phi)
-			one_or_more_termination_prob = self.one_or_more_terminate_prob(self.n_agents, self.terminations)
-			update_target += self.discount*((1.-one_or_more_termination_prob)*current_values[self.last_jointOption] + one_or_more_termination_prob*np.max(current_values))
+			current_Q_option = self.qbigomega.getQvalue(joint_state, self.last_joint_option)
+			beta = self.terminationProbOfAtLeastOneAgent(joint_state, self.last_joint_option)
+			#termination = self.terminations[self.last_options].pmf(Phi)
+			update_target += self.discount * ((1.- beta) * current_Q_option + beta * self.qbigomega.getQvalue(
+				joint_state, None, self.last_joint_option))
 
-		# Update values upon arrival if desired
-		tderror = update_target - self.value(self.last_Phi, self.last_jointOption, self.last_jointAction)
-		self.weights[self.last_Phi, self.last_jointOption, self.last_jointAction] += self.lr*tderror
+		tderror = update_target - self.getQvalue(self.last_joint_state, self.last_joint_option, self.last_joint_action)
+		# import pdb; pdb.set_trace()
+		self.weights[self.last_joint_state][self.last_joint_option][self.last_joint_action] += self.lr*tderror
 
-		self.last_Phi = phi
-		self.last_jointOption = joint_option
-		self.last_jointAction = joint_action
+		self.last_joint_state = joint_state
+		self.last_joint_option = joint_option
+		self.last_joint_action = joint_action
+		
