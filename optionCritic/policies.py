0c13800f2192fb2a9bdd7de4678092f1dd396723
sumanabasu <sumana.basu21@gmail.com>
2019-01-14 19:39:17 -0500

    not final, couple of TODOs to be addressed. But can be used as a placeholder

--- optionCritic/policies.py
+++ optionCritic/policies.py
@@ -4,65 +4,52 @@ from modelConfig import params
 
 
 class SoftmaxPolicy:
-	def __init__(self, n_states, n_choices, temp=params['policy']['temperature']):
+	def __init__(self, weights, temp=params['policy']['temperature']):
 		'''
-		
-		:param n_states: number of encoded individual states
-		:param n_choices: choices over which pmf spreads choice can be either number of primitive actions or options
 		:param temp: lower temperature means uniform distribution, higher means delta
 		'''
 		self.rng = params['train']['seed']
-		self.weights = np.zeros((n_states, n_choices))  # we assume that n_features and
-		# n_actions for all agents are same
+		
+		self.weights = weights
+		# weights is a dictionary keeping track of Q(s,o) values. This is the weight dictionary of IntraOption Q
+		# learning
 		self.temp = temp
 	
-	def value(self, phi, action=None):
-		'''
-		:param phi: state of the agent. Ranges from 1 to 103 (excluding walls)
-		:param action:
-		:return:
-		'''
-		if action is None:
-			value = np.sum(self.weights[phi, :], axis=0)
-		value = np.sum(self.weights[phi, action], axis=0)
-		return value
+	def getQvalue(self, agent_state, action=None):
+		# 1. select all rows where agent_state appears
+		agent_state_keys = [s for s in self.weights.keys() if agent_state in s]
+		# print(agent_state_keys)
+		# print(len(agent_state_keys))
+		
+		# 2. for each option:
+		# 		for each row:
+		# 			calculate the sum of Q values where option appears in joint_options for that row
+		
+		Q = np.zeros((params['agent']['n_options']))
+		
+		for option in range(params['agent']['n_options']):
+			for joint_state in agent_state_keys:
+				# print(joint_state)
+				option_keys = [o for o in self.weights[joint_state].keys() if option in o]
+				# print(option_keys)
+				for o in option_keys:
+					Q[option] += self.weights[joint_state][o]	#TODO : need to average instead of sum
+		# print(Q)
+		
+		# TODO : mask Q for unavailable options
 	
-	def pmf(self, phi):
-		v = self.value(phi) / self.temp
+		return Q
+		
+	
+	def pmf(self, agent_state):
+		v = self.getQvalue(agent_state) / self.temp
 		pmf = np.exp(v - logsumexp(v))
 		return pmf
 	
-	def sample(self, phi):
-		sample_action = int(self.rng.choice(self.weights.shape[1], p=self.pmf(phi)))
-		return sample_action
-
-
-class EgreedyPolicy:
-	def __init__(self, rng, n_features, n_actions, epsilon):
-		self.rng = rng
-		self.epsilon = epsilon
-		self.weights = np.zeros((n_features, n_actions))
-	
-	def value(self, phi, action=None):
-		if action is None:
-			value = np.sum(self.weights[phi, :], axis=0)
-		value = np.sum(self.weights[phi, action], axis=0)
-		return value
-	
-	def sample(self, phi):
-		if self.rng.uniform() < self.epsilon:
-			sample_action = int(self.rng.randint(self.weights.shape[1]))
-		sample_action = int(np.argmax(self.value(phi)))
-		return sample_action
-
-
-class FixedActionPolicies:
-	def __init__(self, joint_action, n_actions):
-		self.joint_action = joint_action
-		self.probs = np.eye(n_actions)[joint_action]
-	
-	def sample(self, phi):
-		return self.joint_action
-	
-	def pmf(self, phi):
-		return self.probs
+	def sample(self, joint_state):
+		joint_option = []
+		for agent_state in joint_state:
+			sample_option = int(self.rng.choice(range(params['agent']['n_options']), p=self.pmf(agent_state)))
+			joint_option.append(sample_option)
+			#TODO : make the sampled option unavailable
+		return tuple(joint_option)
