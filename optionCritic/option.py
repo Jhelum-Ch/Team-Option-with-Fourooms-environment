f4cc4a22ee6141ad8f4f0894d6cf0817225346b9
sumanabasu <sumana.basu21@gmail.com>
2019-01-16 00:30:36 -0500

    option creation fixed, mu_weights initialization yet to be fixed

--- optionCritic/option.py
+++ optionCritic/option.py
@@ -1,6 +1,7 @@
 from modelConfig import params
-from optionCritic.policies import SoftmaxPolicy
+from optionCritic.policies import SoftmaxOptionPolicy, SoftmaxActionPolicy
 from optionCritic.termination import SigmoidTermination
+import itertools
 
 class Option:
 	def __init__(self, optionID, optionPolicy, optionTermination):
@@ -12,12 +13,19 @@ class Option:
 		self.available = True
 		
 def createOptions(env):
+	joint_state_list = list(itertools.permutations(env.cell_list, env.n_agents))
+	joint_option_list = list(itertools.permutations(range(params['agent']['n_options']), params['env']['n_agents']))
+	joint_action_list = list(itertools.product(range(len(env.agent_actions)), repeat=params['env']['n_agents']))
+	
+	# mu_policy is the policy over options
+	mu_weights = dict.fromkeys(joint_state_list, dict.fromkeys(joint_option_list, 0))	#TODO: fix this
+	mu_policy = SoftmaxOptionPolicy(mu_weights)
+	
 	options = []
 	for i in range(params['agent']['n_options']):
-		options.append(Option(i, SoftmaxPolicy(len(env.cell_list), len(env.agent_actions)), SigmoidTermination(len(env.cell_list))))
-		
-	# mu_policy is the policy over options
-	mu_policy = SoftmaxPolicy(len(env.cell_list), params['agent']['n_options'])
+		options.append(Option(i, SoftmaxActionPolicy(len(env.cell_list), len(env.agent_actions)), SigmoidTermination(
+			len(
+			env.cell_list))))	#TODO create weight matrix for joint state and actions instead of individual
 		
 	return options, mu_policy
 		
