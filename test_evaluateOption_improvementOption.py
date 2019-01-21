40c6a49a0eb9b28a5e60e2bb647671b021b597ea
Jhelum Chakravorty <jhelum.here@gmail.com>
2019-01-21 11:54:16 -0500

    Modify test_evaluateOption_improvementOption.py

--- test_evaluateOption_improvementOption.py
+++ test_evaluateOption_improvementOption.py
@@ -10,13 +10,14 @@ import optionCritic.gradients as grads
 env = FourroomsMA()
 avail_options, mu_policies = createOptions(env)
 
-joint_state_list = list(itertools.permutations(env.cell_list, env.n_agents))
+joint_state_list = set([tuple(np.sort(s)) for s in env.states_list])
 joint_option_list = list(itertools.permutations(range(params['agent']['n_options']), params['env']['n_agents']))
 joint_action_list = list(itertools.product(range(len(env.agent_actions)), repeat=params['env']['n_agents']))
 
 # mu_policy is the policy over options
-mu_weights = dict.fromkeys(joint_state_list, dict.fromkeys(joint_option_list, 0))	#TODO: fix this
+mu_weights = dict.fromkeys(joint_state_list, dict.fromkeys(joint_option_list, 0))
 mu_policy = SoftmaxOptionPolicy(mu_weights)
+
 
 pi_policies = [SoftmaxActionPolicy(len(env.cell_list), len(env.agent_actions)) for _ in avail_options]
 
@@ -28,27 +29,30 @@ critic = IntraOptionQLearning(params['env']['discount'], params['doc']['lr_Q'], 
 action_critic = IntraOptionActionQLearning(params['env']['discount'], params['doc']['lr_Q'], option_terminations, critic)
 
 
-agentID = 1
-joint_option = (1,3,2)
-agent_optionID = joint_option[agentID]
-pi_policy_of_agent_option = pi_policies[agent_optionID]
-agent_option_termination = option_terminations[agent_optionID]
+agent = DOC(env, avail_options, mu_policies).env.agents[0]
+print('agent',agent, 'agent option', agent.option)
+
+pi_policy_of_agent_option = pi_policies[agent.option]
+agent_option_termination = option_terminations[agent.option]
 
 intra_option_policy_improvement = grads.IntraOptionGradient(pi_policy_of_agent_option, params['doc']['lr_theta'])
 termination_improvement = grads.TerminationGradient(agent_option_termination, critic, params['doc']['lr_phi'])
 
 
-joint_state = (11,32,43)
-joint_option = (1,3,2)
-joint_action = (0,1,3)
+joint_state = DOC(env, avail_options, mu_policies).s
+joint_option = DOC(env, avail_options, mu_policies).o
+print('jointoption', joint_option)
+joint_action = DOC(env, avail_options, mu_policies).a
 
-agent_state = joint_state[agentID]
-agent_option = joint_option[agentID]
-agent_action = joint_action[agentID]
+# agent_state = joint_state[agentID]
+# agent_option = joint_option[agentID]
+# agent_action = joint_action[agentID]
+agent_state = agent.state
+agent_option = agent.option
+agent_action = agent.action
 
 critic.start(joint_state, joint_option)
 action_critic.start(joint_state, joint_option, joint_action)
 
-evalOption = DOC(env, avail_options, mu_policies).evaluateOption(critic, action_critic, joint_state, joint_option, joint_action, baseline=False)
-imprvOption = DOC(env, avail_options, mu_policies).improveOption_of_agent(agentID, intra_option_policy_improvement, termination_improvement, joint_state, joint_option, joint_action, evalOption)
-
+evalOption = DOC(env, avail_options, mu_policies).evaluateOption(critic, action_critic, option_terminations, baseline=False)
+imprvOption = DOC(env, avail_options, mu_policies).improveOption_of_agent(agent.ID, intra_option_policy_improvement, termination_improvement, evalOption)
