a2c125d177d3d88f90d0e60c1382df7ff3750bc5
sumanabasu <sumana.basu21@gmail.com>
2019-01-24 01:58:24 -0500

    modified chooseOption

--- doc.py
+++ doc.py
@@ -18,7 +18,7 @@ class DOC:
 		self.belief = Belief(env)
 	
 		# Sample a joint state s := vec(s_1,...,s_n) according to belief
-		self.state = self.belief.sampleJointState()
+		self.joint_state = self.belief.sampleJointState()
 		
 		# policy over options
 		self.mu_policy = mu_policy
@@ -27,34 +27,15 @@ class DOC:
 		#TODO: this module needs to be chnaged, since options are chosen as per spftmax policy
 		# Choose joint-option o based on softmax option-policy
 		
-		#select agents randomly to pick options
-		agent_order = [agent.ID for agent in self.env.agents]
-		shuffle(agent_order)
-		# print('agent order :', agent_order)
+		joint_option = self.mu_policy.sample(self.joint_state)
 		
-		joint_option = []
-		#let agents select options from available option pool
-		for agent in agent_order:
-			option_mask = [not(option.available) for option in self.options]
-			# print(option_mask)
+		for option in self.options:
+			option.available = True
 			
-			# pmf = [0, 0, 0.7, 0.1, 0.2]
-			pmf = self.mu_policy.pmf(self.state[agent])
-			pmf = np.ma.masked_array(pmf, option_mask)
-			# print('pmf : ', pmf)
+		for option in joint_option:
+			self.options[option].available = False
 			
-			# select option for agent
-			# TODO : in order to sample option instead of choosing the best one, the masked pdf needs to be re-normalized
-			selected_option_idx = np.argmax(pmf)
-			self.env.agents[agent].option = self.options[selected_option_idx].optionID
-			# print(selected_option_idx)
-		
-			#remove the selected option from available option pool by setting availability to False
-			self.options[selected_option_idx].available = False
-			
-			joint_option.append(selected_option_idx)
-			
-		return tuple(joint_option)
+		return joint_option
 			
 	def chooseAction(self):
 		joint_action = []
