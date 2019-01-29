314eb99fab3db0b3a48fc64071aa7741807b8f72
sumanabasu <sumana.basu21@gmail.com>
2019-01-28 20:16:57 -0500

    arranging doc algorithm

--- train.py
+++ train.py
@@ -1,11 +1,8 @@
-
-from modelConfig import params
-from fourroomsEnv import FourroomsMA
-from optionCritic.belief import Belief
 from optionCritic.option import createOptions
 from doc import DOC
 from modelConfig import params
 from optionCritic.Qlearning import IntraOptionQLearning, IntraOptionActionQLearning
+
 
 class Trainer:
 	def __init__(self, env):
@@ -15,51 +12,71 @@ class Trainer:
 	def train(self):
 		for _ in range(params['train']['n_epochs']):
 			self.trainEpisode()
+			
+	# def putAgentsToGrid(self):
+	# 	idx = 0
+	# 	for agent in self.env.agents:
+	# 		agent.state = params['env']['initial_joint_state'][idx]
+	# 		idx += 1
 	
 	def trainEpisode(self):
 		
-		for _ in range(params['train']['n_episodes']):
+		for episode in range(params['train']['n_episodes']):
+			# put the agent to same initial joint state as long as uses the same random seed set in params['train'][
+			# 'seed'] in modelConfig
+			joint_state = self.env.reset()
+			
+			# # put agents at specific positions on the grid
+			# self.putAgentsToGrid()
+			
 			# create option pool
 			options, mu_policy = createOptions(self.env)
+			# options is a list of option object. Each option object has its own termination policy and pi_policy.
+			# pi_policy for option 0 can be called as	:	options[0].policy.weights
+			# options[0].policy is the object of SoftmaxActionPolicy()
+			# termination for option 0 can be called as	:	options[0].termination.weights
 			
 			terminations = [option.termination for option in options]
 			
-			# a. Start with an initial common belief b0
-			# c. Sample a joint-states:= (s1,...,sn) according to b0
 			doc = DOC(self.env, options, mu_policy)
 			
 			# d. Choose joint-option o based on softmax option-policy mu
-			joint_option = doc.chooseOption()
+			joint_option = doc.chooseOption(joint_state=joint_state)
+			
+			# joint action
+			joint_action = doc.chooseAction()
 			
 			critic = IntraOptionQLearning(discount= params['train']['discount'],
 										  lr= params['train']['lr_critic'],
 										  terminations= terminations,
 										  weights= mu_policy.weights)
 			
+			action_critic = IntraOptionActionQLearning(discount= params['train']['discount'],
+													   lr = params['train']['lr_action_critic'],
+													   terminations=terminations,
+													   qbigomega=critic)
+			
+			critic.start(joint_state, joint_option)
+			action_critic.start(joint_state,joint_option,joint_action)
+			
+			# import pdb; pdb.set_trace()
 			
 			done = False
-			for episode in range(params['env']['episode_length']):
-				# create/reset critic
+			for iteration in range(params['env']['episode_length']):
+				
+				# option evaluation
+				critic_feedback, done = doc.evaluateOption(critic=critic,
+														   action_critic=action_critic,
+														   joint_state=joint_state,
+														   joint_option=joint_option,
+														   joint_action=joint_action,
+														   baseline=False)
+				
+				import pdb; pdb.set_trace()
+				
 				if done:
 					break
-				done = self.trainStep(doc)
-			
-	def trainStep(self, doc):
-		joint_state = doc.state
-		joint_action = doc.chooseAction()
-		
-		doc.evaluateOption(joint_state, joint_action)
-		
-		return done
 	
-			
-			
-			
-	
-	
-	def trainStep(self):
-		#1. Sample s_t ~ b_t
-		pass
 	
 	
 		
