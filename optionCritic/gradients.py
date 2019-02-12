acea25698e21b39c729c62185380e336778907ea
sumanabasu <sumana.basu21@gmail.com>
2019-02-11 21:04:21 -0500

    updated for joint updates

--- optionCritic/gradients.py
+++ optionCritic/gradients.py
@@ -26,26 +26,29 @@
 #         self.pi_policies[joint_optionID].weights[phi, :] -= self.lr*critic*joint_actionPmf
 #         self.pi_policies[joint_optionID].weights[phi, joint_action] += self.lr*critic
 
-
+from modelConfig import params
 
 class TerminationGradient:
-    def __init__(self, termination, critic, lr):
-        self.termination = termination
-        self.critic = critic
-        self.lr = lr
+	def __init__(self, termination, critic, lr=params['train']['lr_phi']):
+		self.termination = termination
+		self.critic = critic
+		self.lr = lr
 
-    def update(self, agentID, joint_state, joint_option): #use joint-state in place of phi
-        phi = joint_state[agentID]
-        magnitudes, directions = self.termination.grad(phi)
-        self.termination.weights[directions] -= \
-                self.lr*magnitudes*(self.critic.getAdvantage(joint_state, joint_option))
+	def update(self, joint_state, joint_option): #use joint-state in place of phi
+		advantage = self.critic.getAdvantage(joint_state, joint_option)
+		for state, option in zip(joint_state, joint_option):
+			# phi = joint_state[agentID]
+			magnitudes, directions = self.termination[option].grad(state)
+			self.termination[option].weights[directions] -= \
+					self.lr*magnitudes*advantage
 # Check this
 class IntraOptionGradient:
-    def __init__(self, pi_policy, lr):
-        self.lr = lr
-        self.pi_policy = pi_policy
+	def __init__(self, pi_policy, lr=params['train']['lr_theta']):
+		self.lr = lr
+		self.pi_policy = pi_policy #as a list of options in use
 
-    def update(self, phi, agent_action, critic): # phi is agent_state
-        agent_actionPmf = self.pi_policy.pmf(phi)
-        self.pi_policy.weights[phi, :] -= self.lr*critic*agent_actionPmf
-        self.pi_policy.weights[phi, agent_action] += self.lr*critic
+	def update(self, joint_state, joint_option, joint_action, critic): # phi is agent_state
+		for state, option, action in zip(joint_state, joint_option, joint_action):
+			agent_actionPmf = self.pi_policy[option].pmf(state)
+			self.pi_policy[option].weights[state, :] -= self.lr*critic*agent_actionPmf
+			self.pi_policy[option].weights[state, action] += self.lr*critic
