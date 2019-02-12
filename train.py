b42eae02a36c674e3d8a373d886de68fb124a9da
sumanabasu <sumana.basu21@gmail.com>
2019-02-11 21:03:38 -0500

    end to end process, as of now

--- train.py
+++ train.py
@@ -2,6 +2,8 @@ from optionCritic.option import createOptions
 from doc import DOC
 from modelConfig import params
 from optionCritic.Qlearning import IntraOptionQLearning, IntraOptionActionQLearning, AgentQLearning
+from distributed.belief import MultinomialDirichletBelief
+from optionCritic.gradients import TerminationGradient, IntraOptionGradient
 import numpy as np
 
 
@@ -23,12 +25,13 @@ class Trainer(object):
 	def trainEpisode(self):
 		
 		for episode in range(params['train']['n_episodes']):
-			# put the agent to same initial joint state as long as uses the same random seed set in params['train'][
-			# 'seed'] in modelConfig
+			# put the agents to the same initial joint state as long as the random seed set in params['train'][
+			# 'seed'] in modelConfig remains unchanged
 			joint_state = self.env.reset()
+			joint_observation = joint_state
 			
-			# # put agents at specific positions on the grid
-			# self.putAgentsToGrid()
+			belief = MultinomialDirichletBelief(self.env, joint_observation)
+			sampled_joint_state= joint_state
 			
 			# create option pool
 			options, mu_policy = createOptions(self.env)
@@ -38,11 +41,16 @@ class Trainer(object):
 			# termination for option 0 can be called as	:	options[0].termination.weights
 			
 			terminations = [option.termination for option in options]
+			pi_policies = [option.policy for option in options]
 			
 			doc = DOC(self.env, options, mu_policy)
 			
 			# d. Choose joint-option o based on softmax option-policy mu
-			joint_option = doc.chooseOption(joint_state=joint_state)
+			joint_option = doc.initializeOption(joint_state=joint_state)
+			
+			# make the elected options unavailable
+			for option in joint_option:
+				options[option].available = False
 			
 			# joint action
 			joint_action = doc.chooseAction()
@@ -57,38 +65,67 @@ class Trainer(object):
 													   terminations=terminations,
 													   qbigomega=critic)
 			
-			x = [option.policy.weights for option in options]
-			# print(x[0].shape)
-			# import numpy as np; p = np.stack((x[:]), axis = 0)
-			# print(p.shape)
-			
-			# agent_q = AgentQLearning(discount=params['train']['discount'],
-			# 						 lr = params['train']['lr_agent_q'],
-			# 						 weights=np.stack(([option.policy.weights for option in options][:]), axis = 0))
-			
 			agent_q = AgentQLearning(discount=params['train']['discount'],
 									 lr=params['train']['lr_agent_q'],
 									 options=options)
 			
-			
 			critic.start(joint_state, joint_option)
 			action_critic.start(joint_state,joint_option,joint_action)
 			agent_q.start(joint_state, joint_option, joint_action)
-			#print(agent_q.value(joint_state, joint_option, joint_action))
 			
-			# import pdb; pdb.set_trace()
+			termination_gradient = TerminationGradient(terminations, critic)
+			intra_option_policy_gradient = IntraOptionGradient(pi_policies)
 			
 			done = False
+			cum_reward = 0
 			for iteration in range(params['env']['episode_length']):
+				print('Episode : ', episode, 'Iteration : ', iteration, 'Cumulative Reward : ', cum_reward)
+				# iv
+				joint_action = doc.chooseAction()
+				
+				# v
+				reward, next_joint_state, done, _ = self.env.step(joint_action)
+				cum_reward += reward
+				
+				# vi - absorbed in broadcastBasedOnQ function of Broadcast class
+				
+				# vii - viii
+				broadcasts = doc.toBroadcast(next_true_joint_state=next_joint_state,
+											 sampled_curr_joint_state=sampled_joint_state,
+											 joint_option=joint_option,
+											 done=done,
+											 critic=critic,
+											 reward=reward)
+				
+				# ix
+				next_joint_observation = self.env.get_observation(broadcasts)
+				
+				# x - critic evaluation
+				critic_feedback = doc.evaluateOption(critic=critic,
+													 action_critic=action_critic,
+													 joint_state=joint_state,
+													 joint_option=joint_option,
+													 joint_action=joint_action,
+													 reward=reward,
+													 done=done,
+													 baseline=False)
 				
 				
-				# option evaluation
-				critic_feedback, done = doc.evaluateOption(critic=critic,
-														   action_critic=action_critic,
-														   joint_state=joint_state,
-														   joint_option=joint_option,
-														   joint_action=joint_action,
-														   baseline=False)
+				# xi A
+				doc.improveOption(policy_obj=intra_option_policy_gradient,
+								  termination_obj=termination_gradient,
+								  joint_state=sampled_joint_state,
+								  joint_option=joint_option,
+								  joint_action=joint_action,
+								  critic_feedback=critic_feedback
+								   )
+				
+				# xi B
+				joint_option = doc.chooseOptionOnTermination(options, joint_option, sampled_joint_state)
+				
+				joint_state = next_joint_state
+				joint_observation = next_joint_observation
+				sampled_joint_state = belief.sampleJointState(joint_observation) # iii
 				
 				if done:
 					break
