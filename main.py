04e7f5e1cc271f83f1396146a5623a31c4384ecb
sumanabasu <sumana.basu21@gmail.com>
2019-02-14 02:18:32 -0500

    experimental setup added

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 from fourroomsEnv import FourroomsMA
-from modelConfig import params
+from modelConfig import params, paths
 from utils.unitTest import testCreateOption, testQ, testSoftmaxOptionPolicy #testActionSelection, testIntraOptionQLearning
 from train import Trainer
+import time
+import os
 
 def main():
+	timestr = time.strftime("%Y%m%d-%H%M%S")
+	base_folder = paths['output']['base_folder']
+	expt_folder = base_folder + timestr
+	if not os.path.exists(expt_folder):
+		os.mkdir(expt_folder)
+	
+	print('Run : {}\n'.format(timestr))
+	
 	env = FourroomsMA(n_agents=params['env']['n_agents'],
 					  goal_reward = params['env']['goal_reward'],
 					  broadcast_penalty =params['env']['broadcast_penalty'],
 					  collision_penalty = params['env']['collision_penalty'])
 	
-	trainer = Trainer(env)
+	trainer = Trainer(env, expt_folder)
 	trainer.trainEpisode()
-	
-	# testCreateOption(env)
-	# testOptionSelection(env)
-	#testActionSelection(env)
-	# testIntraOptionQLearning(env)
-	#print(env.states_list)
-	#print(len(env.states_list))
-	
-	# testQ(env)
-	# testSoftmaxOptionPolicy(env)
-	# print(env.currstate)
-	
 	
 	
 if __name__ == '__main__':
