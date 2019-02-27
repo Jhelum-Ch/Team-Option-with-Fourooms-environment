667a3c9525f281534718a2071082d0b8b5d355f8
Jhelum Chakravorty <jhelum.here@gmail.com>
2019-02-27 10:29:06 -0500

    Update modelConfig.py

--- modelConfig.py
+++ modelConfig.py
@@ -1,31 +1,70 @@
+# import numpy as np
+
+# params = {
+# 	'env'	:	{
+# 		'discount'          : 0.9,
+# 		'n_agents'			:	3,
+# 		'goal_reward' 		:	 1,
+# 		'broadcast_penalty'	:  -0.01,
+# 		'collision_penalty' :  -0.01,
+# 		'initial_joint_state' : (11,31,21)
+# 	},
+# 	'agent'	:	{
+# 		'n_options'	:	5,
+# 		'n_actions' :   4
+# 	},
+# 	'policy'	:	{
+# 		'temperature'	: 0.5
+# 	},
+# 	'train': {
+# 		'n_epochs'		: 	50,
+# 		'n_episodes'	:	100,
+# 		'n_steps' 		: 	100,
+# 		'seed'			:	np.random.RandomState(1234)
+# 	},
+	
+# 	'doc'	:	{
+# 		'lr_theta'	: 0.01,
+# 		'lr_phi'	: 0.01,
+# 		'lr_Q'     : 0.01
+# 	}
+# }
+
 import numpy as np
 
 params = {
 	'env'	:	{
-		'discount'          : 0.9,
 		'n_agents'			:	3,
 		'goal_reward' 		:	 1,
 		'broadcast_penalty'	:  -0.01,
 		'collision_penalty' :  -0.01,
-		'initial_joint_state' : (11,31,21)
+		'episode_length'	: 	500
+		#'initial_joint_state'	:	(11, 31, 21)
 	},
 	'agent'	:	{
 		'n_options'	:	5,
-		'n_actions' :   4
+		'n_actions'	:	4
 	},
 	'policy'	:	{
 		'temperature'	: 0.5
 	},
 	'train': {
-		'n_epochs'		: 	50,
+		'n_runs'		: 	1,
+		# 'n_epochs'		: 	50,
 		'n_episodes'	:	100,
-		'n_steps' 		: 	100,
-		'seed'			:	np.random.RandomState(1234)
-	},
-	
-	'doc'	:	{
-		'lr_theta'	: 0.01,
-		'lr_phi'	: 0.01,
-		'lr_Q'     : 0.01
+		'n_steps' 		: 	500,
+		'seed'			:	np.random.RandomState(1234),
+		'discount'		:	0.1,
+		'lr_critic'		: 	0.001,	#alpha_Q
+		'lr_action_critic'	: 0.001,
+		'lr_agent_q'		: 0.001,
+		'lr_theta'			: 0.001,
+		'lr_phi'			: 0.001
+	}
+}
+
+paths = {
+	'output'	:	{
+		'base_folder'	:	'/home/ml/jchakr1/teamOptionResults/'
 	}
 }
