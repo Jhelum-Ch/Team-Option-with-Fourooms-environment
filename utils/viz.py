0e1a6b16c74c11fecf4aa68f17c56f95636691af
sumanabasu <sumana.basu21@gmail.com>
2019-03-11 23:35:34 -0400

    belief error plot added

--- utils/viz.py
+++ utils/viz.py
@@ -3,6 +3,7 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 import os
+import numpy as np
 
 def plotReward(cum_reward, xlabel, ylabel, location, title):
 
@@ -17,7 +18,16 @@ def plotReward(cum_reward, xlabel, ylabel, location, title):
 	plt.legend()
 	
 	plt.savefig(os.path.join(location, title), bbox_inches="tight")
+
+def calcErrorInBelief(env, true_joint_state, sampled_joint_state):
+	error = 0
+	for true_state, sampled_state in zip(true_joint_state, sampled_joint_state):
+		# print('true state :', true_state, 'tocell :', env.tocellcoord[true_state])
+		# print('sampled state :', sampled_state, 'tocell :', env.tocellcoord[sampled_state])
+		error += np.linalg.norm([i - j for (i, j) in zip(env.tocellcoord[true_state], env.tocellcoord[
+			sampled_state])], 2)
 	
-#TODO : Visualize belief
+	return error
+
 
 #TODO : visualize options
