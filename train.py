2c2b34e9d4e2b9fd3d8227e572231a57ba62b7cd
sumanabasu <sumana.basu21@gmail.com>
2019-04-11 18:13:05 -0400

    neighborhood code added

--- train.py
+++ train.py
@@ -21,13 +21,14 @@ class Trainer(object):
 		for _ in range(params['train']['n_runs']):
 			# put the agents to the same initial joint state as long as the random seed set in params['train'][
 			# 'seed'] in modelConfig remains unchanged
-			joint_state = self.env.reset()
+			#joint_state = self.env.reset()
 			
 			
 			alpha = 0.001 * np.ones(len(self.env.states_list))
 			self.belief = MultinomialDirichletBelief(self.env, alpha)
 
 			# deliberation cost 
+
 			# eta = params['train']['deliberation_cost']
 		
 			
@@ -81,6 +82,7 @@ class Trainer(object):
 		avg_belief_error = []
 		iterations = 0
 		switches = 0
+
 		for episode in range(params['train']['n_episodes']):
 			print('Episode : ', episode)
 			# # put the agents to the same initial joint state as long as the random seed set in params['train'][
@@ -94,19 +96,8 @@ class Trainer(object):
 			# belief = MultinomialDirichletBelief(self.env, joint_observation)
 			sampled_joint_state = joint_state
 			old_feasible_states = joint_state
-			#
-			# # create option pool
-			# options, mu_policy = createOptions(self.env)
-			# # options is a list of option object. Each option object has its own termination policy and pi_policy.
-			# # pi_policy for option 0 can be called as	:	options[0].policy.weights
-			# # options[0].policy is the object of SoftmaxActionPolicy()
-			# # termination for option 0 can be called as	:	options[0].termination.weights
-			#
-			# terminations = [option.termination for option in options]
-			# pi_policies = [option.policy for option in options]
-			#
-			# doc = DOC(self.env, options, mu_policy)
-			#
+			
+			
 			# d. Choose joint-option o based on softmax option-policy mu
 			joint_option = self.doc.initializeOption(joint_state=joint_state)
 
@@ -116,21 +107,7 @@ class Trainer(object):
 
 			# joint action
 			joint_action = self.doc.chooseAction()
-			#
-			# critic = IntraOptionQLearning(discount= params['train']['discount'],
-			# 							  lr= params['train']['lr_critic'],
-			# 							  terminations= terminations,
-			# 							  weights= mu_policy.weights)
-			#
-			# action_critic = IntraOptionActionQLearning(discount= params['train']['discount'],
-			# 										   lr = params['train']['lr_action_critic'],
-			# 										   terminations=terminations,
-			# 										   qbigomega=critic)
-			#
-			# agent_q = AgentQLearning(discount=params['train']['discount'],
-			# 						 lr=params['train']['lr_agent_q'],
-			# 						 options=options)
-			
+
 			self.critic.start(joint_state, joint_option)
 			self.action_critic.start(joint_state,joint_option,joint_action)
 			self.agent_q.start(joint_state, joint_option, joint_action)
@@ -146,8 +123,10 @@ class Trainer(object):
 			c = 0.0
 			
 			for iteration in range(params['env']['episode_length']):
+
 				print('Iteration : ', iteration, 'Cumulative Reward : ', cum_reward, 'Discovered Goals :',
 					  self.env.discovered_goals)
+
 				# iv
 				joint_action = self.doc.chooseAction()
 				
@@ -173,15 +152,21 @@ class Trainer(object):
 											 reward = reward)
 				
 				reward += np.sum([i * self.env.broadcast_penalty for i in broadcasts])
+
 				cum_reward = reward + params['env']['discount'] * cum_reward
 				
 				# ix
 				joint_observation = self.env.get_observation(broadcasts)
 				
+				self.belief.update(joint_observation, old_feasible_states)
+				sampled_joint_state = self.belief.sampleJointState()  # iii
+
+				estimated_next_joint_state = self.estimate_next_joint_state(joint_observation,sampled_joint_state)
+				
 				# x - critic evaluation
 				critic_feedback = self.doc.evaluateOption(critic=self.critic,
 													 action_critic=self.action_critic,
-													 joint_state=joint_state,
+													 joint_state=estimated_next_joint_state, # this should be sampled_next_joint_state, s'_k in the algo
 													 joint_option=joint_option,
 													 joint_action=joint_action,
 													 reward=reward,
@@ -192,15 +177,16 @@ class Trainer(object):
 				# xi A
 				self.doc.improveOption(policy_obj=self.intra_option_policy_gradient,
 								  termination_obj=self.termination_gradient,
-								  joint_state=sampled_joint_state,
+								  joint_state=estimated_next_joint_state, #this should be sampled_next_joint_state, s'_k in the algo
 								  joint_option=joint_option,
 								  joint_action=joint_action,
 								  critic_feedback=critic_feedback
 								   )
 				
 				# xi B
+				
 				next_joint_option, switch = self.doc.chooseOptionOnTermination(self.options, joint_option,
-																		 sampled_joint_state)
+																			   sampled_joint_state) #TODO: should condition on sampled joint state
 				switches += switch
 				# change_in_options = [currJO != nextJO for (currJO,nextJO) in zip(joint_option,next_joint_option)]
 
@@ -217,18 +203,21 @@ class Trainer(object):
 
 				prev_joint_obs = joint_observation
 
+
 				prev_joint_action = joint_action
 				
-				self.belief.update(joint_observation,old_feasible_states)
-				sampled_joint_state = self.belief.sampleJointState() # iii
+				# self.belief.update(joint_observation,old_feasible_states)
+				# sampled_joint_state = self.belief.sampleJointState() # iii
 				
 				true_state_tocells = ([self.env.tocellcoord[s] for s in joint_state])
 				sampled_state_tocells = ([self.env.tocellcoord[s] for s in sampled_joint_state])
-				print('true joint state : ', true_state_tocells, 'sampled joint state :', sampled_state_tocells)
+				# print('true joint state : ', true_state_tocells, 'sampled joint state :', sampled_state_tocells)
 				
+
 				old_feasible_states = self.belief.new_feasible_state(old_feasible_states,joint_observation)
 				
-				belief_error.append(calcErrorInBelief(self.env, joint_state, sampled_joint_state))
+				belief_error_step = calcErrorInBelief(self.env, joint_state, sampled_joint_state)
+				belief_error.append(belief_error_step)
 				
 				itr_reward.append(cum_reward)
 				if not iteration%30 or done:
@@ -242,7 +231,9 @@ class Trainer(object):
 				iterations += 1
 				self.writer.add_scalar('reward_in_iteration', cum_reward, iterations)
 				self.writer.add_scalar('broadcast_in_iteration', np.sum(broadcasts), iterations)
+
 				self.writer.add_scalar('option switches', switches, iterations)
+
 				
 				if done:
 					break
@@ -252,6 +243,7 @@ class Trainer(object):
 				
 				self.writer.add_scalar('Critic_Q_itr', critic_Q, iteration)
 				self.writer.add_scalar('Action_Critic_Q-itr', action_critic_Q, iteration)
+				self.writer.add_scalar('Belief_Error_itr', belief_error_step, iteration)
 					
 			sum_of_rewards_per_episode.append(itr_reward[-1])
 			plotReward(sum_of_rewards_per_episode, 'episodes', 'sum of rewards', self.expt_folder,
@@ -278,6 +270,34 @@ class Trainer(object):
 			
 			
 			#TODO: save checkpoint
+
+
+	def estimate_next_joint_state(self, joint_observation, sampled_joint_state):
+		sampled_joint_state = tuple(np.sort(sampled_joint_state))
+		res = np.zeros(len(joint_observation))
+		for i in range(len(joint_observation)):
+			if joint_observation[i] is not None:
+				if self.env.occupancy[tuple(self.env.tocellcoord[joint_observation[i][0]] + self.env.directions[
+					joint_observation[i][1]])] ==1:
+					idx = np.random.choice(len(self.env.empty_adjacent(self.env.tocellcoord[joint_observation[i][0]])))
+					# print('idx',idx)
+					chosen_cell = self.env.empty_adjacent(self.env.tocellcoord[joint_observation[i][0]])[idx]
+					res[i] = self.env.tocellnum[chosen_cell]
+				else:
+					res[i] = self.env.tocellnum[tuple(self.env.tocellcoord[joint_observation[i][0]] +
+												self.env.directions[joint_observation[i][1]])]
+					
+			else:
+				idx = np.random.choice(len(self.env.empty_adjacent(self.env.tocellcoord[sampled_joint_state[i]])))
+				# print('neighbour',type(self.env.empty_adjacent(self.env.tocellcoord[sampled_joint_state[i]])))
+				chosen_cell = self.env.empty_adjacent(self.env.tocellcoord[sampled_joint_state[i]])[idx]
+				res[i] = self.env.tocellnum[chosen_cell]
+		
+		res = tuple([int(r) for r in res])
+		return res
+
+
+
 	
 		
 	
