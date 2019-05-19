275eed04acd9cfa2a78f091759002644d8ade131
Jhelum Chakravorty <jhelum.here@gmail.com>
2019-05-19 13:35:50 -0400

    Modify modelConfig.py

--- modelConfig.py
+++ modelConfig.py
@@ -2,7 +2,7 @@ import numpy as np
 
 params = {
 	'env'	:	{
-		'n_agents'			:	3,
+		'n_agents'			:	2,
 		'goal_reward' 		:	 1,
 		'broadcast_penalty'	:  -0.02,
 		'collision_penalty' :  -0.01,
@@ -13,25 +13,25 @@ params = {
 		#'initial_joint_state'	:	(11, 31, 21)
 	},
 	'agent'	:	{
-		'n_options'	:	5,
+		'n_options'	:	3,
 		'n_actions'	:	4
 	},
 	'policy'	:	{
 		'temperature'	: 1.0
 	},
 	'train': {
-		'n_runs'		: 	5,
+		'n_runs'		: 	1,
 		# 'n_epochs'		: 	50,
 		'n_episodes'	:	200,
-		'n_steps' 		: 	1000,
+		'n_steps' 		: 	1500,
 		'seed'			:	1234,
 		# 'discount'		:	0.1,
-		'lr_critic'		: 	0.01,	#alpha_Q
-		'lr_action_critic'	: 0.01,
-		'lr_agent_q'		: 0.01,
+		'lr_critic'		: 	0.05,	#alpha_Q
+		'lr_action_critic'	: 0.05,
+		'lr_agent_q'		: 0.05,
 		'lr_theta'			: 0.01,
 		'lr_phi'			: 0.01,
-		'deliberation_cost' : 100
+		'deliberation_cost' : 0.1
 	}
 }
 
@@ -40,5 +40,6 @@ seed = np.random.RandomState(params['train']['seed'])
 paths = {
 	'output'	:	{
 		'base_folder'	:	'/home/ml/jchakr1/teamOptionResults/'
+		#'base_folder'	:  '/network/home/chakravj/teamOptionResults'
 	}
 }
